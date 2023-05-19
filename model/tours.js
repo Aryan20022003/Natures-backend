74b0cfe54a862df67862ab7cd7407f7eb0f7a91e
Aryan20022003 <sharmakumaranil24121968@gmail.com>
2023-05-19 13:39:53 +0530

    controller is seperated to its differnet folder

---
+++ model/tours.js
@@ -0,0 +1,24 @@
+// create a model and a schema out of it
+const mongoose = require('mongoose');
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, 'name must be there'],
+      unique: true,
+    },
+    rating: {
+      type: Number,
+      default: 4,
+    },
+    price: {
+      type: Number,
+      require: [true, 'price must be there'],
+    },
+  },
+  { collection: 'tours' }
+);
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
--- server.js
+++ server.js
@@ -17,42 +17,21 @@ mongoose
     console.error('Error connecting to MongoDB:', error);
   });
 
-// create a model and a schema out of it
-const tourSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      require: [true, 'name must be there'],
-      unique: true,
-    },
-    rating: {
-      type: Number,
-      default: 4,
-    },
-    price: {
-      type: Number,
-      require: [true, 'price must be there'],
-    },
-  },
-  { collection: 'tours' }
-);
 
-const Tour = mongoose.model('Tour', tourSchema);
+// const testTour = new Tour({
+//   name: 'Forest trek',
+//   rating: 4.7,
+//   price: 500,
+// });
 
-const testTour = new Tour({
-  name: 'Forest trek',
-  rating: 4.7,
-  price: 500,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// testTour
+//   .save()
+//   .then((doc) => {
+//     console.log(doc);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 const port = process.env.Port;
 
