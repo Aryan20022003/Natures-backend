f1beeef80ab0b992f53cdb27e33dfd03b311caf4
Aryan20022003 <sharmakumaranil24121968@gmail.com>
2023-05-21 00:40:20 +0530

    converted the method into class for reusability

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,33 +1,113 @@
 const { json } = require('express');
 const Tour = require('./../model/tours');
 
-const getAllTour = async (req, resp) => {
-  try {
-    const { difficulty } = req.query;
+class API {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+  find() {
+    const { difficulty } = this.queryString;
     console.log({ difficulty });
     // Extract the 'difficulty' parameter from the query string
+    if (difficulty) {
+      this.query = this.query.find({ difficulty });
+    }
+    return this;
+  }
+  sorting() {
+    if (this.queryString.sort) {
+      console.log(this.queryString.sort);
 
-    let data = Tour.find({ difficulty });
-    // Filter the data based on the 'difficulty' parameter
-
-    // Sorting
-    if (req.query.sort) {
-      console.log(req.query.sort);
-      // Extract the 'sort' parameter from the query string
-
-      let sortBy = JSON.stringify(req.query.sort).split(',').join(' ');
-      // Convert the 'sort' parameter to a string, replace commas with spaces
-      // Example: "price,rating" => "price rating"
+      let sortBy = JSON.stringify(this.queryString.sort).split(',').join(' ');
 
       sortBy = JSON.parse(sortBy);
-      // Parse the string back to an object
-      // Example: "price rating" => { price: 1, rating: -1 }
 
-      data = data.sort(sortBy);
-      // Apply sorting based on the 'sortBy' object
+      this.query = this.query.sort(sortBy);
+      return this;
     }
+  }
+  fieldRequested() {
+    //3 returning only those field requested by user
+    if (this.queryString.select) {
+      console.log(this.queryString.select);
+      let toSelect = this.queryString.select.split(',').join(' ');
+      this.query = this.query.select(toSelect);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+  async paging() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
 
-    data = await data;
+    data = data.limit(limit).skip((page - 1) * limit);
+
+    const totalDataCount = await this.query.countDocuments();
+    if ((page - 1) * limit >= totalDataCount) {
+      throw new Error('Invalid page number');
+    }
+  }
+}
+
+const getAllTour = async (req, resp) => {
+  try {
+    // const { difficulty } = req.query;
+    // console.log({ difficulty });
+    // // Extract the 'difficulty' parameter from the query string
+
+    // //1->find method
+    // let data=Tour.find();
+    // if (difficulty) {
+    //   data = Tour.find({ difficulty });
+    // }
+
+    // Filter the data based on the 'difficulty' parameter
+
+    // 2 Sorting
+    // if (req.query.sort) {
+    //   console.log(req.query.sort);
+    //   // Extract the 'sort' parameter from the query string
+
+    //   let sortBy = JSON.stringify(req.query.sort).split(',').join(' ');
+    //   // Convert the 'sort' parameter to a string, replace commas with spaces
+    //   // Example: "price,rating" => "price rating"
+
+    //   sortBy = JSON.parse(sortBy);
+    //   // Parse the string back to an object
+    //   // Example: "price rating" => { price: 1, rating: -1 }
+
+    //   data = data.sort(sortBy);
+    //   // Apply sorting based on the 'sortBy' object
+    // }
+
+    //3 returning only those field requested by user
+    // if (req.query.select) {
+    //   console.log(req.query.select);
+    //   let toSelect = req.query.select.split(',').join(' ');
+    //   data.select(toSelect);
+    // } else {
+    //   data.select('-__v');
+    // }
+    //4 paging and limit
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+
+    // data = data.limit(limit).skip((page - 1) * limit);
+
+    // const totalDataCount = await Tour.countDocuments();
+    // if ((page - 1) * limit >= totalDataCount) {
+    //   throw new Error('Invalid page number');
+    // }
+
+    const data = await( new API(Tour.find(), req.query)
+      .find()
+      .fieldRequested()
+      .paging()
+      .sorting()).query;
+    // data = await data;
+
     // Await the execution of the 'data' query
 
     resp.status(201).json({
@@ -36,7 +116,6 @@ const getAllTour = async (req, resp) => {
         tour: data,
       },
     });
-    // Send a JSON response with the sorted and filtered 'data'
   } catch (err) {
     resp.status(400).json({
       status: 'failed',
